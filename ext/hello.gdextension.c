745c8192e3a7e4bd27250f6a0b24452d0e62daf3
ParadoxV5 <paradox.ver5@gmail.com>
2023-05-07 21:44:45 -0600

    `#CString_to_StringName`

--- ext/hello.gdextension.c
+++ ext/hello.gdextension.c
@@ -3,10 +3,39 @@
 #include "include/godot/gdextension_interface.h"
 #include "hello.gdextension.h"
 
+#define gdextension_hash_print 2648703342 // fetch from `include/extension_api.json`
+
+const GDExtensionInterface* INTERFACE;
+GDExtensionPtrConstructor StringName_from_String;
+GDExtensionPtrDestructor destroy_StringName;
+GDExtensionPtrDestructor destroy_String;
+
+GDExtensionStringName CString_to_StringName(const char* cstring) {
+  void* string;
+  INTERFACE->string_new_with_latin1_chars(&string, cstring);
+  GDExtensionStringName string_name;
+  StringName_from_String(&string_name, (GDExtensionConstTypePtr[1]) {&string});
+  destroy_String(string);
+  return string_name;
+}
+
 void hello_gdextension_initialize(void *userdata, GDExtensionInitializationLevel p_level) {
+  if(p_level == GDEXTENSION_INITIALIZATION_SCENE) {
+    // Save used pointers
+    StringName_from_String = INTERFACE->variant_get_ptr_constructor(GDEXTENSION_VARIANT_TYPE_STRING_NAME, 2); // 2nd constructor
+    destroy_StringName = INTERFACE->variant_get_ptr_destructor(GDEXTENSION_VARIANT_TYPE_STRING_NAME);
+    destroy_String = INTERFACE->variant_get_ptr_destructor(GDEXTENSION_VARIANT_TYPE_STRING);
+    
+  }
   printf("initialized at level %u\n", p_level);
 }
-void hello_gdextension_deinitialize(void *userdata, GDExtensionInitializationLevel p_level){
+void hello_gdextension_deinitialize(void *userdata, GDExtensionInitializationLevel p_level) {
+  if(p_level == GDEXTENSION_INITIALIZATION_SCENE) {
+    // Unset stale pointers
+    StringName_from_String = NULL;
+    destroy_StringName = NULL;
+    destroy_String = NULL;
+  }
   printf("deinitialized at level %u\n", p_level);
 }
 
@@ -19,5 +48,8 @@ GDExtensionBool hello_gdextension_main(
   // Set up de/initialization functions
   r_initialization->initialize = &hello_gdextension_initialize;
   r_initialization->deinitialize = &hello_gdextension_deinitialize;
+  // Save the GDExtensionInterface
+  INTERFACE = p_interface;
+  // Success
   return true;
 }
--- ext/hello.gdextension.h
+++ ext/hello.gdextension.h
@@ -1,6 +1,9 @@
 #ifndef HELLO_GDEXTENSION_H
 #define HELLO_GDEXTENSION_H
 
+typedef void* GDExtensionStringName
+void* String_to_StringName(const char* string);
+
 GDExtensionBool hello_gdextension_main(
   const GDExtensionInterface *p_interface,
   GDExtensionClassLibraryPtr p_library,
