7bc2c7e260e89823a427812a53f704e7348b051b
ParadoxV5 <paradox.ver5@gmail.com>
2023-05-07 22:41:16 -0600

    `#gdextension_print`

--- ext/hello.gdextension.c
+++ ext/hello.gdextension.c
@@ -9,19 +9,29 @@ GDExtensionStringName CString2StringName(const char* cstring) {
   GDExtensionString string;
   INTERFACE->string_new_with_latin1_chars(&string, cstring);
   GDExtensionStringName string_name;
-  StringName_from_String(&string_name, (GDExtensionConstTypePtr[1]) {&string});
+  stringName_from_String(&string_name, (GDExtensionConstTypePtr[1]) {&string});
   destroy_String(string);
   return string_name;
 }
 
+void gdextension_print(const char* cstring) {
+  GDExtensionString string;
+  INTERFACE->string_new_with_utf8_chars(&string, cstring);
+  GDExtensionVariant variant;
+  variant_from_String(&variant, &string);
+  global_print(NULL, (GDExtensionConstVariantPtr[1]) { &variant }, 1);
+  INTERFACE->variant_destroy(&variant);
+  destroy_String(&string);
+}
+
 void hello_gdextension_initialize(void *userdata, GDExtensionInitializationLevel p_level) {
   if(p_level == GDEXTENSION_INITIALIZATION_SCENE) {
-    // Save used pointers
-    StringName_from_String = INTERFACE->variant_get_ptr_constructor(GDEXTENSION_VARIANT_TYPE_STRING_NAME, 2); // Constructor #2
+    // Save API pointers
+    stringName_from_String = INTERFACE->variant_get_ptr_constructor(GDEXTENSION_VARIANT_TYPE_STRING_NAME, 2); // Constructor #2
     destroy_StringName = INTERFACE->variant_get_ptr_destructor(GDEXTENSION_VARIANT_TYPE_STRING_NAME);
     destroy_String = INTERFACE->variant_get_ptr_destructor(GDEXTENSION_VARIANT_TYPE_STRING);
+    variant_from_String = INTERFACE->get_variant_from_type_constructor(GDEXTENSION_VARIANT_TYPE_STRING);
     // Fetch GDScript methods
-    
     GDExtensionStringName stringName_print = CString2StringName("print");
     global_print = INTERFACE->variant_get_ptr_utility_function(&stringName_print, HASH_print);
     destroy_StringName(&stringName_print);
@@ -32,7 +42,7 @@ void hello_gdextension_initialize(void *userdata, GDExtensionInitializationLevel
 void hello_gdextension_deinitialize(void *userdata, GDExtensionInitializationLevel p_level) {
   if(p_level == GDEXTENSION_INITIALIZATION_SCENE) {
     // Unset stale pointers
-    StringName_from_String = NULL;
+    stringName_from_String = NULL;
     destroy_StringName = NULL;
     destroy_String = NULL;
     global_print = NULL;
--- ext/hello.gdextension.h
+++ ext/hello.gdextension.h
@@ -3,17 +3,21 @@
 
 typedef void* GDExtensionString;
 typedef void* GDExtensionStringName;
+typedef void* GDExtensionVariant;
 
 // GDExtension interface pointer
 extern const GDExtensionInterface* INTERFACE;
 // GDExtension API pointers
-extern GDExtensionPtrConstructor StringName_from_String;
+extern GDExtensionPtrConstructor stringName_from_String;
 extern GDExtensionPtrDestructor destroy_StringName;
 extern GDExtensionPtrDestructor destroy_String;
+extern GDExtensionVariantFromTypeConstructorFunc variant_from_String;
 // Library Functions
 extern GDExtensionPtrUtilityFunction global_print;
 
-void* CString2StringName(const char* string);
+GDExtensionStringName CString2StringName(const char* cstring);
+
+void gdextension_print(const char* string);
 
 GDExtensionBool hello_gdextension_main(
   const GDExtensionInterface *p_interface,
