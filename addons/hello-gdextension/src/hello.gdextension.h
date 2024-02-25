808ba281b5c7845457e5fc04bd6b4d3ca97cd240
ParadoxV5 <paradox.ver5@gmail.com>
2024-02-25 14:20:33 -0700

    Remodel for Godot 4.2 (part 1: header)

--- addons/hello-gdextension/src/hello.gdextension.h
+++ addons/hello-gdextension/src/hello.gdextension.h
@@ -7,31 +7,21 @@
   In GDScript, everything is a Variant;
   but in C/++ lands, “int”, “String” and etc. refer to the internal data which Variants wrap for GDScript convenience.
 */
-typedef void* GDExtensionString;
-typedef void* GDExtensionStringName;
-typedef uint8_t GDExtensionVariant[40];
+typedef uint_least32_t GDExtensionString[2], GDExtensionStringName[2], GDExtensionVariant[10];
 
-// GDExtension interface pointer
-extern GDExtensionInterfaceGetProcAddress GetProcAddress;
-// GDExtension API pointers
-extern GDExtensionPtrConstructor stringName_from_String;
-extern GDExtensionPtrDestructor destroy_StringName;
-extern GDExtensionPtrDestructor destroy_String;
-extern GDExtensionVariantFromTypeConstructorFunc variant_from_String;
-// GDExtension pointers
+// GDExtension Variant API
 extern GDExtensionInterfaceStringNewWithLatin1Chars string_new_with_latin1_chars;
-extern GDExtensionInterfaceStringNewWithUtf8Chars string_new_with_utf8_chars;
+extern GDExtensionPtrDestructor string_destroy;
+extern GDExtensionVariantFromTypeConstructorFunc variant_from_string;
 extern GDExtensionInterfaceVariantDestroy variant_destroy;
-// Library Functions
-extern GDExtensionPtrUtilityFunction global_print;
-
-GDExtensionStringName CString2StringName(const char* cstring);
+// Godot Engine Library
+extern GDExtensionPtrUtilityFunction global_scope_print;
 
 void gdextension_print(const char* string);
 
 __attribute__((unused)) GDExtensionBool hello_gdextension_main(
-  GDExtensionInterfaceGetProcAddress p_get_proc_address,
-  __attribute__((unused)) GDExtensionClassLibraryPtr p_library,
+  GDExtensionInterfaceGetProcAddress get_proc_address,
+  __attribute__((unused)) GDExtensionClassLibraryPtr library,
   GDExtensionInitialization* r_initialization
 );
 
